e9b47280a497669cf944e5f9835ea390b85b51f6
Ryan de Sao Jose <ryan.desaojose@mail.utoronto.ca>
2021-04-03 13:20:32 -0400

    Fixed setting user based on session in restricted queries

--- cubingcafe/server/graphql/index.js
+++ cubingcafe/server/graphql/index.js
@@ -12,17 +12,24 @@ const isAuthenticated = async (resolve, source, args, context, info) => {
   return resolve(source, args, context, info);
 };
 
+const setUserInFilter = async (resolve, source, args, context, info) => {
+  const user = context.session.username;
+  if (args.filter) {
+    if (args.filter.user && args.filter.user !== user) {
+      return Promise.reject(new Error('Access denied'));
+    }
+    args.filter.user = user;
+  } else {
+    args.filter = { user };
+  }
+  return resolve(source, args, context, info);
+};
+
 schemaComposer.Query.addFields({
   userById: UserTC.getResolver('findById'),
   userMany: UserTC.getResolver('findMany'),
-  sessionMany: SessionTC.getResolver('findMany', [isAuthenticated]).wrapResolve(next => rp => {
-    rp.args.filter = { user: rp.context.session.username };
-    return next(rp);
-  }),
-  solveMany: SolveTC.getResolver('findMany', [isAuthenticated]).wrapResolve(next => rp => {
-    rp.args.filter = { user: rp.context.session.username };
-    return next(rp);
-  }),
+  sessionMany: SessionTC.getResolver('findMany', [isAuthenticated, setUserInFilter]),
+  solveMany: SolveTC.getResolver('findMany', [isAuthenticated, setUserInFilter]),
   getSession: SessionTC.getResolver('getSession', [isAuthenticated])
 });
 
