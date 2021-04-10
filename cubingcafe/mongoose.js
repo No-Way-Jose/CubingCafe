781948b16413dfc8596a7e80a4cc3db7deedb563
Ryan de Sao Jose <ryan.desaojose@mail.utoronto.ca>
2021-04-10 16:44:24 -0400

    Added interval to continuously attempt connecting to Mongo if initial connection fails

--- cubingcafe/mongoose.js
+++ cubingcafe/mongoose.js
@@ -1,13 +1,25 @@
 const mongoose = require('mongoose');
 
 const mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost/cubingCafeDB';
-mongoose
-  .connect(mongoUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .catch(error => console.error.bind(console, error));
 
-mongoose.connection.on('open', () => console.log.bind(console, 'Mongo connected'));
+const connect = () => {
+  console.log('Attempting Mongo connection');
+  mongoose
+    .connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .catch(error => {
+      console.error(error);
+      console.log('Initial connection to Mongo failed, retrying in 15 seconds');
+      setTimeout(connect, 15000);
+    });
+};
+
+mongoose.connection.on('open', () => console.log('Mongo connected'));
+
+mongoose.connection.on('disconnected', () => console.log('Mongo disconnected.'));
+
+connect();
 
 module.exports = mongoose;
